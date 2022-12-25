365b16cde3b5a3312a1269e5f2fad869dce47e41
PrzemekS <przemeksekula@gmail.com>
2022-12-25 12:10:05 +0100

    feat: Shooting animations (51-52)

--- Assets/Scripts/Actions/ShootAction.cs
+++ Assets/Scripts/Actions/ShootAction.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 
 public class ShootAction : BaseAction
 {
+    public event EventHandler<OnShootEventArgs> OnShoot;
+
+    public class OnShootEventArgs : EventArgs
+    {
+        public Unit targetUnit;
+        public Unit shootingUnit;
+    }
+
     private enum State
     {
         Aiming,
@@ -74,6 +82,10 @@ public class ShootAction : BaseAction
 
     private void Shoot()
     {
+        OnShoot?.Invoke(this, new OnShootEventArgs {
+             targetUnit = targetUnit, 
+             shootingUnit = unit 
+             });        
         targetUnit.Damage();
     }
 
