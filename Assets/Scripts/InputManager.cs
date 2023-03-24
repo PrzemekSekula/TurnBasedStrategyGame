767b008cb486ca9f08df135905a7237ef58c546a
PrzemekS <przemeksekula@gmail.com>
2023-03-24 23:52:40 +0100

    feat: Input system added (l. 79)

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,10 +1,13 @@
+#define USE_NEW_INPUT_SYSTEM 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
     public static InputManager Instance { get; private set; }
+    private PlayerInputActions playerInputActions;
 
     private void Awake() 
     {
@@ -15,20 +18,34 @@ public class InputManager : MonoBehaviour
             return;
         }
         Instance = this;
+
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
     }
 
     public Vector3 GetMouseScreenPosition()
     {
+#if USE_NEW_INPUT_SYSTEM
+        return Mouse.current.position.ReadValue();
+#else        
         return Input.mousePosition;
+#endif
     }
 
-    public bool IsMouseButtonDown(int button = 0)
+    public bool IsMouseButtonDownThisFrame()
     {
-        return Input.GetMouseButtonDown(button);
+#if USE_NEW_INPUT_SYSTEM
+        return playerInputActions.Player.Click.WasPressedThisFrame();
+#else
+        return Input.GetMouseButtonDown(0);
+#endif       
     }
 
     public Vector2 GetCameraMoveVector()
     {
+#if USE_NEW_INPUT_SYSTEM
+        return playerInputActions.Player.CameraMovement.ReadValue<Vector2>();
+#else  
         Vector2 inputMoveDir = new Vector2(0, 0);
         if (Input.GetKey(KeyCode.W))
         {
@@ -48,11 +65,14 @@ public class InputManager : MonoBehaviour
         }
 
         return inputMoveDir;
-
+#endif
     }
 
     public float GetCameraRotateAmount()
     {
+#if USE_NEW_INPUT_SYSTEM
+        return playerInputActions.Player.CameraRotate.ReadValue<float>();
+#else
         float rotationAmount = 0f;
         if (Input.GetKey(KeyCode.Q))
         {
@@ -64,10 +84,15 @@ public class InputManager : MonoBehaviour
         }
 
         return rotationAmount;
+#endif
     }
 
     public float GetCameraZoomAmount()
     {
+#if USE_NEW_INPUT_SYSTEM
+        return playerInputActions.Player.CameraZoom.ReadValue<float>();
+#else
+
         float zoomAmount = 0f;
         if (Input.mouseScrollDelta.y > 0)
         {
@@ -79,6 +104,7 @@ public class InputManager : MonoBehaviour
         }
 
         return zoomAmount;
+#endif
     }
 
 }
